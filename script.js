6c98f5703a4e1a8704f8139f77ccb659ff287182
Josevall <josevalles9314@gmail.com>
2022-09-10 03:30:05 -0400

    burger menu

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const burger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
 burger.addEventListener('click', toggleMobileMenu);
+
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
 }
+
+const itemFromList = document.querySelector('.mobile-menu-list');
+itemFromList.addEventListener('click', closeBurger);
+
+function closeBurger() {
+    mobileMenu.classList.add('inactive');
+}
+
